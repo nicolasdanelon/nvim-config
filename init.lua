b094fe60f00f06b6d7b6bb06d07a154acdf3db2d
Nicolas Danelon <nicolasdanelon@gmail.com>
2022-12-23 16:49:44 -0300

    nord theme iterm2

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+-- An arctic, north-bluish clean and elegant iTerm2 color scheme. 
+-- https://github.com/arcticicestudio/nord-iterm2
 require("nicolasdanelon.plugins-setup")
 require("nicolasdanelon.core.options")
 require("nicolasdanelon.core.keymaps")
